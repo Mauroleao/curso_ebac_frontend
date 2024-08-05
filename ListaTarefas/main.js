154bebf5298c344eb316ee532d1506e5020ed828
mauro <mauro_leao@hotmail.com>
2024-08-05 08:58:34 -0300

    "Finalizando o app"

--- ListaTarefas/main.js
+++ ListaTarefas/main.js
@@ -1,41 +1,20 @@
-const form = document.getElementById('form-cliente');
-const nome = [];
-const telefone = [];
+$(document).ready(function() {
+    $('#form-tarefa').on('submit', function(e) {
+        e.preventDefault();
+        adicionaTarefa();
+    });
 
-let linhas = '';
+    function adicionaTarefa() {
+        const inputTarefa = $('#tarefa');
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
+        if ($.trim(inputTarefa.val()) !== '') {
+            const li = $('<li></li>').text(inputTarefa.val());
+            li.on('click', function() {
+                $(this).toggleClass('strikethrough');
+            });
+            $('#lista-tarefas').append(li);
 
-    adicionaLinha();
-    atualizaTabela();
-    
-
-});
-
-function adicionaLinha(){
-    const inputNomeCliente =  document.getElementById('nome-cliente');
-    const inputTelefoneCliente =  document.getElementById('telefone-cliente');
-
-    if (nome.includes(inputNomeCliente.value)) {
-        alert(`O Nome: ${inputNomeCliente.value} já foi inserido`); 
-    } else {
-        nome.push(inputNomeCliente.value);
-        telefone.push(parseFloat(inputTelefoneCliente.value));
-    
-    let linha = '<tr>';
-    linha += `<td>${inputNomeCliente.value}</td>`;
-    linha += `<td>${inputTelefoneCliente.value}</td>`;
-    linha += '</tr>';
-    
-    linhas += linha;   
+            inputTarefa.val('');
+        }
     }
-    
-    inputNomeCliente.value = '';
-    inputTelefoneCliente.value = '';
-}
-
-function atualizaTabela() {
-    const corpoTabela = document.querySelector('tbody');
-    corpoTabela.innerHTML = linhas;
-}
+});
